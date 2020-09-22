e263bd4a856675fc0446cb02f7a74e2e2ec70063
Jamie Laundon <jamielaundon@users.noreply.github.com>
2020-09-22 21:48:24 +0100

    Update AssemblyInfo.cs

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeyLightSwitch")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Key Light Switch")]
+[assembly: AssemblyDescription("Control Elgato Key Light Air")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Atos")]
-[assembly: AssemblyProduct("KeyLightSwitch")]
-[assembly: AssemblyCopyright("Copyright © Atos 2020")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Key Light Switch")]
+[assembly: AssemblyCopyright("Copyright © Jamie Laundon 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
