3e56cf3177758beee4a1bdb914187f6b4ac12d2f
Jamie Laundon <jamielaundon@users.noreply.github.com>
2020-09-22 21:43:51 +0100

    Basic get/put functionality complete version, no settings

--- src/Program.cs
+++ src/Program.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,9 +21,83 @@ namespace KeyLightSwitch
 {
     class Program
     {
+
+        static HttpClient client = new HttpClient();
+
+        static async Task<Uri> SetLightAsync(KeyLightAir light, string path)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                path, light);
+            response.EnsureSuccessStatusCode();
+
+            return response.Headers.Location;
+        }
+
+        static async Task<KeyLightAir> GetLightAsync(string path)
+        {
+            KeyLightAir light = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                light = await response.Content.ReadAsAsync<KeyLightAir>();
+            }
+            return light;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("KeyLightSwitch");
+            Console.WriteLine("Key Light Switch");
+            RunAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task RunAsync()
+        {
+            // TODO read from settings
+            client.BaseAddress = new Uri("http://IP-ADDRESS-HERE:9123/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                var url = "elgato/lights";
+                KeyLightAir keylight = new KeyLightAir();
+
+                // Get the current light status
+                keylight = await GetLightAsync(url);
+
+                //Turn off if already on
+                if (keylight.lights[0].on == 1)
+                {
+                    Console.WriteLine("Turning off...");
+                    keylight.lights[0].on = 0;
+                }
+                else
+                {
+                    // Create a new light
+                    // TODO read from settings
+                    Console.WriteLine("Turning on...");
+                    Light light = new Light
+                    {
+                        brightness = 20,
+                        temperature = 250,
+                        on = 1
+                    };
+
+                    keylight = new KeyLightAir();
+                    keylight.lights = new List<Light>();
+                    keylight.lights.Add(light);
+                    keylight.numberOfLights = keylight.lights.Count();
+                }
+
+                var result = await SetLightAsync(keylight,url);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
